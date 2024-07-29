d329dc6fddbbfff8770768f0912dbf362217bfb3
Nikita Varivodin <kujijieep@gmail.com>
2024-07-29 10:24:09 +0300

    /

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,12 @@ import vsharp from "vite-plugin-vsharp"
 import data from "./data.json"
 
 export default {
+	base: '',
 	plugins: [
 		handlebars({
 			context: data,
 			partialDirectory: resolve(__dirname, 'src/partials'),
 		}),
-		vsharp({
-			".webp": {
-				effort: 6,
-				quality: 80,
-				force: true
-			},
-		}),
+		vsharp(),
 	],
 }
